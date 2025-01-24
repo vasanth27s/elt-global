c0262d09acbb397853ec421af96db63c7d98feae
vasanth <vasanth2003s@gmail.com>
2025-01-24 23:22:40 +0530

    elt-global

--- src/app/components/examComponents/QuestionNumber.tsx
+++ src/app/components/examComponents/QuestionNumber.tsx
@@ -1,87 +1,88 @@
-"use client"
+"use client";
 
-
-import React, { useEffect, useState } from 'react'
-import { useAppContext } from '@/app/utils/AppContext'
-import NumberSkeleton from '../skeleton/NumberSkeleton'
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "@/app/utils/AppContext";
+import NumberSkeleton from "../skeleton/NumberSkeleton";
 
 const QuestionNumber = () => {
+  const [data, setData] = useState([]);
+  const [selected, setSelected] = useState<number>(1);
+  const [skeleton, setSkeleton] = useState(false);
 
-  const [data, setData] = useState([])
-  const [selected, setSelected] = useState<number>(1)
-  const[skeleton,setSkeleton] = useState(false)
   const API_URL = process.env.NEXT_PUBLIC_API_URL;
-  console.log(API_URL)
 
-  const { setQno, setQid, answered, flagged, qno } = useAppContext()
-
-
+  const { setQno, setQid, answered, flagged, qno } = useAppContext();
 
   useEffect(() => {
-    const fetchdata = async () => {
-
+    const fetchData = async () => {
       try {
-        setSkeleton(true)
-        const reponse = await fetch(`${API_URL}/api/questions`)
-        const data = await reponse.json()
-        setData(data)
+        setSkeleton(true);
+        const response = await fetch(`${API_URL}/api/questions`);
+        const data = await response.json();
+        setData(data);
       } catch (error) {
-        console.log(error)
-      }finally{
-        setSkeleton(false)
+        console.error("Error fetching questions:", error);
+      } finally {
+        setSkeleton(false);
       }
+    };
 
-    }
-    fetchdata()
-  }, [])
+    fetchData();
+  }, [API_URL]);
 
   useEffect(() => {
-    setSelected(qno)
-  }, [qno])
-
-
+    setSelected(qno);
+  }, [qno]);
 
   const questionClick = (qno: number, qid: string) => {
-    setSelected(qno)
-    setQno(qno)
-    setQid(qid)
-  }
+    setSelected(qno);
+    setQno(qno);
+    setQid(qid);
+  };
 
   return (
-    <div className='p-2 w-screen md:h-full md:w-1/4 '>
-      <div className=' bg-white h-full px-5 py-6 rounded-md'>
-        <div className='pb-5 border-b-2'>
-          <p className='font-medium'>Overview</p>
+    <div className="p-2 w-screen md:h-full md:w-1/4">
+      <div className="bg-white h-full px-5 py-6 rounded-md">
+        <div className="pb-5 border-b-2">
+          <p className="font-medium">Overview</p>
         </div>
-        {!skeleton &&<div className='flex flex-wrap gap-2 mt-5'>
-          {data?.map((item: any, index: number) => {
-            const itemId = item?.id;
-            let backgroundColor = 'bg-slate-100';
+        {!skeleton && (
+          <div className="flex flex-wrap gap-2 mt-5">
+            {data?.map((item: any, index: number) => {
+              const itemId = item?.id;
+              let backgroundColor = "bg-slate-100";
 
-            if (answered[itemId]) {
-              backgroundColor = 'bg-green-600';
-            }
-            if (flagged?.includes(itemId)) {
-              backgroundColor = 'bg-orange-400';
-            }
-            if (answered[itemId] && flagged?.includes(itemId)) {
-              backgroundColor = 'bg-green-600';
-            }
-            return (<div key={index + 1} className={`w-7 h-7 md:w-10 md:h-10 rounded-full flex justify-center items-center cursor-pointer ${selected === index + 1 ? 'border-orange-400 border-2' : ''}   ${backgroundColor} inline-block`} onClick={() => {
-              if (itemId) {
-                questionClick(index + 1, itemId);
+              if (answered[itemId]) {
+                backgroundColor = "bg-green-600";
               }
-            }}>
-              {index + 1}
-            </div>)
-          })}
-
-        </div>}
-        {skeleton && <NumberSkeleton/>}
-        
+              if (flagged?.includes(itemId)) {
+                backgroundColor = "bg-orange-400";
+              }
+              if (answered[itemId] && flagged?.includes(itemId)) {
+                backgroundColor = "bg-green-600";
+              }
+              return (
+                <div
+                  key={index + 1}
+                  className={`w-7 h-7 md:w-10 md:h-10 rounded-full flex justify-center items-center cursor-pointer ${
+                    selected === index + 1 ? "border-orange-400 border-2" : ""
+                  } ${backgroundColor} inline-block`}
+                  onClick={() => {
+                    if (itemId) {
+                      questionClick(index + 1, itemId);
+                    }
+                  }}
+                >
+                  {index + 1}
+                </div>
+              );
+            })}
+          </div>
+        )}
+        {skeleton && <NumberSkeleton />}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuestionNumber
+export default QuestionNumber;
--- src/app/data/questions.ts
+++ src/app/data/questions.ts
@@ -1,320 +1,217 @@
-
-
- 
 interface Question {
-  id: string;
-  question: string;
-  options: Array<{ id: string; option: string }>;
-}
-
-const questions: Question[] = [
-  {
+    id: string;
+    question: string;
+    options: Array<{ id: string; option: string }>;
+  }
+  
+  const questions: Question[] = [
+    {
       id: 'q1',
-      question: 'Which planet is known as the Red Planet?',
+      question: 'What is the primary color of the sky on a clear day?',
       options: [
-          { id: 'o1', option: 'Earth' },
-          { id: 'o2', option: 'Mars' },
-          { id: 'o3', option: 'Venus' },
-          { id: 'o4', option: 'Jupiter' },
+        { id: 'o1', option: 'Blue' },
+        { id: 'o2', option: 'Green' },
+        { id: 'o3', option: 'Red' },
+        { id: 'o4', option: 'Yellow' },
       ],
-  },
-  {
+    },
+    {
       id: 'q2',
-      question: 'What is the capital city of Australia?',
+      question: 'Which fruit is known as the king of fruits?',
       options: [
-          { id: 'o1', option: 'Sydney' },
-          { id: 'o2', option: 'Melbourne' },
-          { id: 'o3', option: 'Canberra' },
-          { id: 'o4', option: 'Brisbane' },
+        { id: 'o1', option: 'Apple' },
+        { id: 'o2', option: 'Banana' },
+        { id: 'o3', option: 'Mango' },
+        { id: 'o4', option: 'Pineapple' },
       ],
-  },
-  {
+    },
+    {
       id: 'q3',
-      question: 'Which is the largest ocean on Earth?',
+      question: 'Which animal is known for having black and white stripes?',
       options: [
-          { id: 'o1', option: 'Atlantic Ocean' },
-          { id: 'o2', option: 'Indian Ocean' },
-          { id: 'o3', option: 'Arctic Ocean' },
-          { id: 'o4', option: 'Pacific Ocean' },
+        { id: 'o1', option: 'Zebra' },
+        { id: 'o2', option: 'Tiger' },
+        { id: 'o3', option: 'Panda' },
+        { id: 'o4', option: 'Cow' },
       ],
-  },
-  {
+    },
+    {
       id: 'q4',
-      question: 'What is the square root of 144?',
+      question: 'What is the name of the tallest mountain in the world?',
       options: [
-          { id: 'o1', option: '10' },
-          { id: 'o2', option: '12' },
-          { id: 'o3', option: '14' },
-          { id: 'o4', option: '16' },
+        { id: 'o1', option: 'Mount Everest' },
+        { id: 'o2', option: 'K2' },
+        { id: 'o3', option: 'Kangchenjunga' },
+        { id: 'o4', option: 'Lhotse' },
       ],
-  },
-  {
+    },
+    {
       id: 'q5',
-      question: 'Which country hosted the 2016 Summer Olympics?',
+      question: 'What is the boiling point of water at sea level?',
       options: [
-          { id: 'o1', option: 'China' },
-          { id: 'o2', option: 'Brazil' },
-          { id: 'o3', option: 'United Kingdom' },
-          { id: 'o4', option: 'Russia' },
+        { id: 'o1', option: '100°C' },
+        { id: 'o2', option: '90°C' },
+        { id: 'o3', option: '110°C' },
+        { id: 'o4', option: '120°C' },
       ],
-  },
-  {
+    },
+    {
       id: 'q6',
-      question: 'What is the hardest natural substance on Earth?',
+      question: 'What is the capital city of Germany?',
       options: [
-          { id: 'o1', option: 'Gold' },
-          { id: 'o2', option: 'Iron' },
-          { id: 'o3', option: 'Diamond' },
-          { id: 'o4', option: 'Platinum' },
+        { id: 'o1', option: 'Berlin' },
+        { id: 'o2', option: 'Munich' },
+        { id: 'o3', option: 'Hamburg' },
+        { id: 'o4', option: 'Cologne' },
       ],
-  },
-  {
+    },
+    {
       id: 'q7',
-      question: "Who wrote the play 'Romeo and Juliet'?",
+      question: 'What is the fastest land animal?',
       options: [
-          { id: 'o1', option: 'Charles Dickens' },
-          { id: 'o2', option: 'William Shakespeare' },
-          { id: 'o3', option: 'Leo Tolstoy' },
-          { id: 'o4', option: 'Mark Twain' },
+        { id: 'o1', option: 'Cheetah' },
+        { id: 'o2', option: 'Lion' },
+        { id: 'o3', option: 'Horse' },
+        { id: 'o4', option: 'Leopard' },
       ],
-  },
-  {
+    },
+    {
       id: 'q8',
-      question: 'What is the chemical symbol for water?',
+      question: 'Which gas do humans exhale?',
       options: [
-          { id: 'o1', option: 'H2O' },
-          { id: 'o2', option: 'CO2' },
-          { id: 'o3', option: 'O2' },
-          { id: 'o4', option: 'HO' },
+        { id: 'o1', option: 'Carbon Dioxide' },
+        { id: 'o2', option: 'Oxygen' },
+        { id: 'o3', option: 'Nitrogen' },
+        { id: 'o4', option: 'Helium' },
       ],
-  },
-  {
+    },
+    {
       id: 'q9',
-      question: "Which animal is known as the 'King of the Jungle'?",
+      question: 'Who painted the Mona Lisa?',
       options: [
-          { id: 'o1', option: 'Elephant' },
-          { id: 'o2', option: 'Tiger' },
-          { id: 'o3', option: 'Lion' },
-          { id: 'o4', option: 'Cheetah' },
+        { id: 'o1', option: 'Leonardo da Vinci' },
+        { id: 'o2', option: 'Vincent van Gogh' },
+        { id: 'o3', option: 'Pablo Picasso' },
+        { id: 'o4', option: 'Claude Monet' },
       ],
-  },
-  {
+    },
+    {
       id: 'q10',
-      question: 'In which year did the Titanic sink?',
+      question: 'What is the main ingredient of bread?',
       options: [
-          { id: 'o1', option: '1910' },
-          { id: 'o2', option: '1912' },
-          { id: 'o3', option: '1914' },
-          { id: 'o4', option: '1920' },
+        { id: 'o1', option: 'Flour' },
+        { id: 'o2', option: 'Rice' },
+        { id: 'o3', option: 'Corn' },
+        { id: 'o4', option: 'Barley' },
       ],
-  },
-  {
+    },
+    {
       id: 'q11',
-      question: 'What is the smallest prime number?',
+      question: 'What is the national animal of the United States?',
       options: [
-          { id: 'o1', option: '1' },
-          { id: 'o2', option: '2' },
-          { id: 'o3', option: '3' },
-          { id: 'o4', option: '5' },
+        { id: 'o1', option: 'Bald Eagle' },
+        { id: 'o2', option: 'Grizzly Bear' },
+        { id: 'o3', option: 'American Bison' },
+        { id: 'o4', option: 'Cougar' },
       ],
-  },
-  {
+    },
+    {
       id: 'q12',
-      question: 'Which planet is the closest to the Sun?',
+      question: 'Which metal is used in making coins and batteries?',
       options: [
-          { id: 'o1', option: 'Venus' },
-          { id: 'o2', option: 'Mercury' },
-          { id: 'o3', option: 'Earth' },
-          { id: 'o4', option: 'Mars' },
+        { id: 'o1', option: 'Nickel' },
+        { id: 'o2', option: 'Iron' },
+        { id: 'o3', option: 'Copper' },
+        { id: 'o4', option: 'Aluminum' },
       ],
-  },
-  {
+    },
+    {
       id: 'q13',
-      question: 'In what year did World War II end?',
+      question: 'What is the capital of Canada?',
       options: [
-          { id: 'o1', option: '1943' },
-          { id: 'o2', option: '1945' },
-          { id: 'o3', option: '1947' },
-          { id: 'o4', option: '1950' },
+        { id: 'o1', option: 'Ottawa' },
+        { id: 'o2', option: 'Toronto' },
+        { id: 'o3', option: 'Vancouver' },
+        { id: 'o4', option: 'Montreal' },
       ],
-  },
-  {
+    },
+    {
       id: 'q14',
-      question: 'What is the capital of Japan?',
+      question: 'What is the speed of light?',
       options: [
-          { id: 'o1', option: 'Beijing' },
-          { id: 'o2', option: 'Seoul' },
-          { id: 'o3', option: 'Tokyo' },
-          { id: 'o4', option: 'Kyoto' },
+        { id: 'o1', option: '299,792 km/s' },
+        { id: 'o2', option: '150,000 km/s' },
+        { id: 'o3', option: '450,000 km/s' },
+        { id: 'o4', option: '600,000 km/s' },
       ],
-  },
-  {
+    },
+    {
       id: 'q15',
-      question: "Which element has the chemical symbol 'O'?",
+      question: 'Which is the national flower of India?',
       options: [
-          { id: 'o1', option: 'Oxygen' },
-          { id: 'o2', option: 'Osmium' },
-          { id: 'o3', option: 'Ozone' },
-          { id: 'o4', option: 'Oxide' },
+        { id: 'o1', option: 'Lotus' },
+        { id: 'o2', option: 'Rose' },
+        { id: 'o3', option: 'Sunflower' },
+        { id: 'o4', option: 'Marigold' },
       ],
-  },
-  {
+    },
+    {
       id: 'q16',
-      question: 'How many sides does a hexagon have?',
+      question: 'What is the chemical symbol for gold?',
       options: [
-          { id: 'o1', option: '4' },
-          { id: 'o2', option: '5' },
-          { id: 'o3', option: '6' },
-          { id: 'o4', option: '8' },
+        { id: 'o1', option: 'Au' },
+        { id: 'o2', option: 'Ag' },
+        { id: 'o3', option: 'Pb' },
+        { id: 'o4', option: 'Fe' },
       ],
-  },
-  {
+    },
+    {
       id: 'q17',
-      question: 'Which is the longest river in the world?',
+      question: 'Which bird is known for mimicking sounds?',
       options: [
-          { id: 'o1', option: 'Amazon' },
-          { id: 'o2', option: 'Nile' },
-          { id: 'o3', option: 'Yangtze' },
-          { id: 'o4', option: 'Mississippi' },
+        { id: 'o1', option: 'Parrot' },
+        { id: 'o2', option: 'Sparrow' },
+        { id: 'o3', option: 'Eagle' },
+        { id: 'o4', option: 'Penguin' },
       ],
-  },
-  {
+    },
+    {
       id: 'q18',
-      question: 'What is the hardest known mineral?',
+      question: 'What is the main gas found in the air we breathe?',
       options: [
-          { id: 'o1', option: 'Quartz' },
-          { id: 'o2', option: 'Diamond' },
-          { id: 'o3', option: 'Corundum' },
-          { id: 'o4', option: 'Topaz' },
+        { id: 'o1', option: 'Nitrogen' },
+        { id: 'o2', option: 'Oxygen' },
+        { id: 'o3', option: 'Carbon Dioxide' },
+        { id: 'o4', option: 'Hydrogen' },
       ],
-  },
-  {
+    },
+    {
       id: 'q19',
-      question: "Who is known as the 'Father of Computers'?",
+      question: 'Which body part pumps blood throughout the human body?',
       options: [
-          { id: 'o1', option: 'Alan Turing' },
-          { id: 'o2', option: 'Charles Babbage' },
-          { id: 'o3', option: 'John von Neumann' },
-          { id: 'o4', option: 'Thomas Edison' },
+        { id: 'o1', option: 'Heart' },
+        { id: 'o2', option: 'Lungs' },
+        { id: 'o3', option: 'Kidneys' },
+        { id: 'o4', option: 'Liver' },
       ],
-  },
-  {
+    },
+    {
       id: 'q20',
-      question: 'Which organ in the human body is primarily responsible for detoxification?',
+      question: 'Which instrument is used to measure temperature?',
       options: [
-          { id: 'o1', option: 'Liver' },
-          { id: 'o2', option: 'Kidney' },
-          { id: 'o3', option: 'Lungs' },
-          { id: 'o4', option: 'Heart' },
+        { id: 'o1', option: 'Thermometer' },
+        { id: 'o2', option: 'Barometer' },
+        { id: 'o3', option: 'Hygrometer' },
+        { id: 'o4', option: 'Anemometer' },
       ],
-  },
-  {
-      id: 'q21',
-      question: 'What is the currency of Japan?',
-      options: [
-          { id: 'o1', option: 'Yuan' },
-          { id: 'o2', option: 'Dollar' },
-          { id: 'o3', option: 'Yen' },
-          { id: 'o4', option: 'Won' },
-      ],
-  },
-  {
-      id: 'q22',
-      question: 'How many continents are there in the world?',
-      options: [
-          { id: 'o1', option: '5' },
-          { id: 'o2', option: '6' },
-          { id: 'o3', option: '7' },
-          { id: 'o4', option: '8' },
-      ],
-  },
-  {
-      id: 'q23',
-      question: 'Who invented the light bulb?',
-      options: [
-          { id: 'o1', option: 'Alexander Graham Bell' },
-          { id: 'o2', option: 'Thomas Edison' },
-          { id: 'o3', option: 'Nikola Tesla' },
-          { id: 'o4', option: 'Benjamin Franklin' },
-      ],
-  },
-  {
-      id: 'q24',
-      question: 'Which is the largest country by land area?',
-      options: [
-          { id: 'o1', option: 'Canada' },
-          { id: 'o2', option: 'China' },
-          { id: 'o3', option: 'Russia' },
-          { id: 'o4', option: 'United States' },
-      ],
-  },
-  {
-      id: 'q25',
-      question: 'Which gas do plants absorb from the atmosphere?',
-      options: [
-          { id: 'o1', option: 'Oxygen' },
-          { id: 'o2', option: 'Carbon Dioxide' },
-          { id: 'o3', option: 'Nitrogen' },
-          { id: 'o4', option: 'Hydrogen' },
-      ],
-  },
-  {
-      id: 'q26',
-      question: 'In which direction does the sun rise?',
-      options: [
-          { id: 'o1', option: 'North' },
-          { id: 'o2', option: 'South' },
-          { id: 'o3', option: 'East' },
-          { id: 'o4', option: 'West' },
-      ],
-  },
-  {
-      id: 'q27',
-      question: 'Which planet in our solar system has the most moons?',
-      options: [
-          { id: 'o1', option: 'Earth' },
-          { id: 'o2', option: 'Saturn' },
-          { id: 'o3', option: 'Jupiter' },
-          { id: 'o4', option: 'Mars' },
-      ],
-  },
-  {
-      id: 'q28',
-      question: 'What is the largest mammal in the world?',
-      options: [
-          { id: 'o1', option: 'Elephant' },
-          { id: 'o2', option: 'Blue Whale' },
-          { id: 'o3', option: 'Great White Shark' },
-          { id: 'o4', option: 'Giraffe' },
-      ],
-  },
-  {
-      id: 'q29',
-      question: 'What is the main ingredient in guacamole?',
-      options: [
-          { id: 'o1', option: 'Avocado' },
-          { id: 'o2', option: 'Tomato' },
-          { id: 'o3', option: 'Lettuce' },
-          { id: 'o4', option: 'Onion' },
-      ],
-  },
-  {
-      id: 'q30',
-      question: 'Which country is famous for the Eiffel Tower?',
-      options: [
-          { id: 'o1', option: 'Germany' },
-          { id: 'o2', option: 'Italy' },
-          { id: 'o3', option: 'France' },
-          { id: 'o4', option: 'Spain' },
-      ],
-  },
-];
-
-
-/**
-* Mock function to simulate fetching questions from a server.
-* Returns a promise that resolves with an array of `Question` objects after a delay of 500 milliseconds.
-*
-* @returns {Promise<Question[]>} A promise that resolves with an array of questions.
-*/
-export const getQuestions = () => new Promise<Question[]>((resolve) => setTimeout(() => resolve(questions), 500));
+    },
+  ];
+  
+  /**
+   * Mock function to simulate fetching questions from a server.
+   * Returns a promise that resolves with an array of `Question` objects after a delay of 500 milliseconds.
+   *
+   * @returns {Promise<Question[]>} 
+   */
+  export const getQuestions = () => new Promise<Question[]>((resolve) => setTimeout(() => resolve(questions), 500));
+  
